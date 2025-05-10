0b17d953b3cad8d97e8480c1743330ce68f92dcc
PineW <2992086988@qq.com>
2025-05-10 17:45:58 +0800

    修复bug

--- src/main/java/com/thfh/repository/ChatMessageRepository.java
+++ src/main/java/com/thfh/repository/ChatMessageRepository.java
@@ -4,7 +4,7 @@ import com.thfh.model.ChatMessage;
 import com.thfh.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.param.Param;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
